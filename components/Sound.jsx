7f62bdc04aa76d1abe5e28dadc185d5454d08710
홍성준 <hackish113@gmail.com>
2021-03-24 23:58:26 +0900

    [improved]: 사운드 테스트

--- components/Sound.jsx
+++ components/Sound.jsx
@@ -1,14 +1,22 @@
-const Sound = (props) => {
-  const onAudioPlay = (event) => {
-    event.target.pause()
+import { useRef, useState, useEffect } from 'react'
 
-    setTimeout(() => {
-      event.target.play()
-    }, 3000)
+const Sound = (props) => {
+  const track = useRef()
+  // const [track, setTrack] = useState()
+  const onAudioPlay = (event) => {
+    // setTrack(event.target)
   }
 
+  useEffect(() => {
+    track.current.pause()
+
+    if (props.play) {
+      track.current.play()
+    }
+  }, [props.play])
+
   return (
-    <audio onCanPlay={onAudioPlay} src={`./sound/${props.src}.mp3`} />
+    <audio ref={track} onCanPlay={onAudioPlay} src={`./sound/${props.src}`} />
   )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,114 @@
+import { useState, useEffect } from 'react'
 import { Sound } from '../components'
+import style from '../styles/Home.module.css'
 
+const minute = 60000
+const tabSize = 80
+const second = minute * tabSize / 60 / 60
+const codeList = [
+  '0', '0s', '1', '1s', '2', '3', '3s', '4', '4s', '5', '5s', '6',
+  '7', '7s', '8', '8s', '9', '10', '10s', '11', '11s', '12', '12s', '13',
+  '14', '14s', '15', '15s', '16', '17', '17s', '18', '18s', '19', '19s', '20',
+]
+const rightSheet = [
+  {
+    sounds: [],
+    timer: 3000,
+  },
+  {
+    sounds: ['10', '8'],
+    timer: second,
+  },
+  {
+    sounds: ['12'],
+    timer: second,
+  },
+  {
+    sounds: ['9', '7'],
+    timer: second * 2,
+  },
+  {
+    sounds: ['8', '6'],
+    timer: second * (6 / 4) * 2,
+  },
+  {
+    sounds: [],
+    timer: second * (6 / 4) * 4,
+  },
+  {
+    sounds: ['10', '8'],
+    timer: second,
+  },
+  {
+    sounds: ['12'],
+    timer: second,
+  },
+  {
+    sounds: ['9', '7'],
+    timer: second * 2,
+  },
+  {
+    sounds: ['8', '6'],
+    timer: second * 4,
+  },
+]
+const leftSheet = [
+  {
+    sounds: [],
+    timer: 3000,
+  },
+  {
+    sounds: [],
+    timer: second * 4,
+  },
+  {
+    sounds: [],
+    timer: second * (6 / 4) * 6,
+  },
+  {
+    sounds: ['5', '1'],
+    timer: second * 2,
+  },
+  {
+    sounds: ['4', '0'],
+    timer: second * 2,
+  },
+]
 const Home = () => {
+  const [rightSoundList, setRightSoundList] = useState([])
+  const [leftSoundList, setLeftSoundList] = useState([])
+  console.log(second * 2, second * (6 / 4) * 4)
+  const onChangeSound = (index, list, fn) => {
+    if (index < list.length) {
+      const { sounds, timer } = list[index]
+
+      fn(sounds)
+
+      setTimeout(() => {
+        onChangeSound(++index, list, fn)
+      }, timer)
+    }
+  }
+
+  useEffect(() => {
+    onChangeSound(0, rightSheet, setRightSoundList)
+    // onChangeSound(0, leftSheet, setLeftSoundList)
+  }, [])
+
   return (
-    <div>
-      <Sound src="0" />
-      <Sound src="1" />
-      <Sound src="2" />
-      <Sound src="3" />
-      <Sound src="4" />
-      <Sound src="5" />
+    <div className={style.container}>
+      {codeList.map((sound) => (
+        <div className={`${style.code} ${rightSoundList.some((item) => item === sound) ? style.on : ''}`} key={sound}>
+          {sound} {`${rightSoundList.some((item) => item === sound)}`}
+          <Sound src={`${sound}.mp3`} play={rightSoundList.some((item) => item === sound)} />
+        </div>
+      ))}
+      {/*{codeList.map((sound) => (*/}
+      {/*  <div key={sound}>*/}
+      {/*    {sound} {`${leftSoundList.indexOf(sound) > 0}`}*/}
+      {/*    <Sound src={`${sound}.mp3`} play={leftSoundList.some((item) => item === sound)} />*/}
+      {/*  </div>*/}
+      {/*))}*/}
     </div>
   )
 }
